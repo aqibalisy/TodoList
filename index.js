4aecaf56c17f4ec5cdd66c420caebbea5835f7f4
Aqib Ali <101803091+aqibalisy@users.noreply.github.com>
2023-09-26 19:31:00 +0530

    logic part completed

--- index.js
+++ index.js
@@ -5,7 +5,27 @@ const app = express();
 const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+let item = []
 
-app.listen(port, ()=>{
-    console.log(`listening on port${port}`)
+app.get('/', (req, res) => {
+  let options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  let today = new Date();
+ let day = today.toLocaleDateString("en-US", options)
+ 
+ res.render('index.ejs', {kindOfDay:day, items: item})
+});
+
+app.post('/', (req, res) => {
+ let newItems = req.body.newItem
+ item.push(newItems)
+ res.redirect("/")
+});
+
+app.listen(port, () => {
+  console.log(`listening on port${port}`);
 });
